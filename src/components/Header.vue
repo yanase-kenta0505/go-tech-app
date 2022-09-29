cad3e37396a4c654412cc88bf59cfa05db1fe651
yanasekenta <ynsknt0505@gmail.com>
2022-09-29 16:59:23 +0900

    ツールバーを作成

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,18 @@
   </v-app-bar>
   <v-card class="mt-16">
     <v-toolbar>
-      <v-icon >mdi-folder-open-outline</v-icon>
+      <v-btn>
+        <v-icon size="30">mdi-folder-open-outline</v-icon>
+      </v-btn>
+      <v-divider vertical color="black" inset></v-divider>
+      <v-btn>
+        <v-icon size="30">mdi-file-download-outline</v-icon>
+      </v-btn>
+      <v-spacer/>
+      <v-btn>
+        <v-icon size="30">mdi-dock-right</v-icon>
+      </v-btn>
+
     </v-toolbar>
   </v-card>
 </template>
